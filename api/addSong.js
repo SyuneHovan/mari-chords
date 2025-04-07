3897f5c44877cbf78722f899af72a0e8baffbba6
Syune Hovhannisyan <syunehovhan@gmail.com>
2025-04-07 18:06:36 +0400

    asa

--- api/addSong.js
+++ api/addSong.js
@@ -27,18 +27,13 @@ export default function handler(req, res) {
     };
 
     songs.push(newSong);
-    let eee = JSON.stringify(songs, null, 2);
+    let data = JSON.stringify(songs, null, 2);
 
     try {
-      // if (process.env.NODE_ENV === "development") {
-
-      // fs.writeFileSync(songsFilePath, eee, "utf8");
-        // const data1 = fs.readFileSync(songsFilePath, "utf8");
-      // }
-      return res.status(200).json({ message: "Song added successfully!", eee });
+      fs.writeFileSync(songsFilePath, data, "utf8");
+      return res.status(200).json({ message: "Song added successfully!", data });
     } catch (error) {
-      return res.status(200).json(eee);
-      // return res.status(500).json({ error: "Failed to save the song" });
+      return res.status(500).json({ error: "Failed to save the song", error });
     }
   } else {
     return res.status(405).json({ error: "Method not allowed" });
