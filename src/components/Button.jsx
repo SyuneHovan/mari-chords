2dab4e8cd9fc84a6f1b605f22ad3b7f1ad9fba9b
Syune Hovhannisyan <syunehovhan@gmail.com>
2025-04-07 17:27:36 +0400

    as

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-export const Button = ({ onClick, children }) => (
-    <button className="p-2 bg-blue-500 text-white rounded" onClick={onClick}>
+export const Button = ({ onClick, children, className }) => (
+    <button className={`p-2 bg-blue-500 text-white rounded ${className}`} onClick={onClick}>
         {children}
     </button>
 );
--- src/pages/SongPage.jsx
+++ src/pages/SongPage.jsx
@@ -3,42 +3,43 @@ import { useParams } from "react-router-dom";
 import { Button } from "../components/Button";
 import songs from "../data/songs.json";
 import { Header } from "./Header";
-import { Col, Row } from "antd";
+import { Col, InputNumber, Row } from "antd";
+import ScrollIcon from "../../public/svgs/scroll";
+import StopIcon from "../../public/svgs/stop";
+import { Input } from "../components/Input";
 
 export const SongPage = () => {
     const { songName } = useParams();
     const song = songs.find(s => s.name === songName);
     const [autoScroll, setAutoScroll] = useState(false);
     const [scrollSpeed, setScrollSpeed] = useState(1);
-
+    
     useEffect(() => {
-        console.log("scrollSpeed", scrollSpeed);
-        
         let interval;
         if (autoScroll) {
             interval = setInterval(() => {
-                window.scrollBy({ top: 1, behavior: "smooth" });
-            }, 100/scrollSpeed);
+                document.getElementById("scrollRoll").scrollBy({ top: scrollSpeed * 2, behavior: "smooth" });
+            }, Math.max(10, 100 / scrollSpeed));    
         }
+        
         return () => clearInterval(interval);
     }, [autoScroll, scrollSpeed]);
-
+    
+    
     return song ? (
     <div>
         <Header />
         <div className="song-page-container">
-            <h1>{song.name} - {song.author}</h1>
-            <Row>
-                <Col span={6}>
+            <h3>{song.name} - {song.author}</h3>
+            <Row gutter={[10, 10]} className="scroll-button">
+                <Col style={{gap:"20px"}}>
+                    <InputNumber type="number" style={{width: "100px"}} defaultValue={1} min={1} max={100} onChange={(num) => setScrollSpeed(num)}/>
                     <Button onClick={() => setAutoScroll(!autoScroll)}>
-                        {autoScroll ? "Stop" : "Start"} Auto-Scroll
+                        {autoScroll ? <StopIcon/> : <ScrollIcon/>} 
                     </Button>
                 </Col>
-                <Col span={1}>
-                    <input type="number" style={{width: "100px"}} onChange={(e) => setScrollSpeed(e.target.value)}/>
-                </Col>
             </Row>
-            <div className="lyrics">
+            <div className="lyrics" id="scrollRoll">
 
                 {song.lyrics.map((line, lineIndex) => (
 
