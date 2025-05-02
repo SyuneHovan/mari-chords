88828bd65435ab685dc129b6b0a6c864457bfa5b
Sue Hovhan <87306378+SyuneHovan@users.noreply.github.com>
2025-05-02 21:07:24 +0000

    ui home, song page

---
+++ public/svgs/song.jsx
@@ -0,0 +1,31 @@
+const SongIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 314 315" fill="none">
+      <g filter="url(#filter0_d_3_28)">
+      <path d="M262 10H30C21.1634 10 14 17.1634 14 26V258C14 266.837 21.1634 274 30 274H262C270.837 274 278 266.837 278 258V26C278 17.1634 270.837 10 262 10Z" fill="#FFF2DF"/>
+      </g>
+      <circle cx="145.5" cy="273.5" r="41.5" fill="#242424"/>
+      <path d="M134 261.158C134 258.029 137.431 256.112 140.096 257.752L161.379 270.848C163.937 272.423 163.912 276.151 161.332 277.69L140.05 290.39C137.383 291.981 134 290.06 134 286.955V261.158Z" fill="#FEF1DE"/>
+      <path d="M104 135C60.1529 100.208 54.315 56.785 55.741 31.1451C55.8563 29.073 57.593 27.5 59.6683 27.5H257C259.209 27.5 261 29.2909 261 31.5V132.725C261 134.135 260.252 135.449 259.03 136.154C203.999 167.925 141.362 164.646 104 135Z" fill="#B46E4C"/>
+      <path d="M183.5 120C183.5 67.7128 222.878 53.3972 257.604 57.5347C259.563 57.768 261 59.4553 261 61.428V181.033C261 182.999 259.585 184.685 257.628 184.883C230.889 187.604 183.5 175.579 183.5 120Z" fill="#8C5B3B"/>
+      <path d="M148.788 151.161C150.788 150.174 153.168 151.189 153.833 153.317C158.938 169.644 160.855 198.377 135.898 210.518C134.021 211.43 131.79 210.56 131.016 208.622C124.764 192.968 121.487 164.637 148.788 151.161Z" fill="#242424"/>
+      <path d="M46.7024 174.862C44.8576 175.483 43.7477 177.389 44.2303 179.275C49.6148 200.31 70.9177 232.698 120.465 218.572C122.459 218.004 123.662 215.956 123.079 213.965C116.678 192.104 94.2004 158.893 46.7024 174.862Z" fill="#242424"/>
+      <path d="M88.4311 111.981C86.4937 111.365 84.4409 112.429 83.8507 114.374C78.801 131.017 78.3647 160.591 110.036 170.976C111.985 171.615 114.063 170.546 114.654 168.582C119.701 151.791 120.127 122.062 88.4311 111.981Z" fill="#242424"/>
+      <defs>
+      <filter id="filter0_d_3_28" x="0" y="0" width="314" height="314" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feMorphology radius="4" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_3_28"/>
+      <feOffset dx="11" dy="15"/>
+      <feGaussianBlur stdDeviation="10.5"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.24 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_3_28"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_3_28" result="shape"/>
+      </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default SongIcon;
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import songs from "../data/songs.json";
 import AddIcon from "../../public/svgs/add";
 import { Col, Row } from 'antd';
 import { Header } from "./Header";
+import WaveIcon from "../../public/svgs/wave";
+import SongIcon from "../../public/svgs/song";
+import Paragraph from "antd/es/typography/Paragraph";
+import CategoryScroll from "../components/CategoryScroll";
 
 
 export const List = () => {
     const [filter, setFilter] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [category, setCategory] = useState([]);
     const [filteredSongs, setFilteredSongs] = useState(songs);
+    const navigate = useNavigate();
 
-    console.log("songs", songs)
+    const handleNavigate = () => {
+        navigate('/AddSong');
+    };
 
     useEffect(() => {
         setFilteredSongs(
@@ -22,6 +30,13 @@ export const List = () => {
                 (selectedCategory === "" || song.category === selectedCategory)
             )
         );
+        setCategory([
+            { value: "", label: "All" },
+            ...[...new Set(songs.map(song => song.category))].map(category => ({
+              value: category,
+              label: category
+            }))
+          ]);
     }, [filter, selectedCategory]);
 
     return (
@@ -34,7 +49,8 @@ export const List = () => {
                             <Input placeholder="Search by name..." onChange={(e) => setFilter(e.target.value)} />
                         </Col>
                         <Col>
-                            <Select options={[{ value: "", label: "All Categories" }, { value: "Pop", label: "Pop" }, { value: "Rock", label: "Rock" }]} onChange={(e) => setSelectedCategory(e.target.value)} />
+                            {/* <Select options={[{ value: "", label: "All Categories" }, { value: "Pop", label: "Pop" }, { value: "Rock", label: "Rock" }]} onChange={(e) => setSelectedCategory(e.target.value)} /> */}
+                            <CategoryScroll options={category} onChange={(e) => setSelectedCategory(e.target.value)} />
                         </Col>
                     </Row>
                 </Col>
@@ -43,11 +59,25 @@ export const List = () => {
                 <Col>
                     <ul>
                         {filteredSongs.map(song => (
-                            <li key={song.name}><Link to={`/song/${song.name}`}>{song.name} - {song.author}</Link></li>
+                            <li key={song.name}>
+                                <Link to={`/song/${song.name}`}>
+                                    <Row className="song" justify={"space-between"}>
+                                        <Col>
+                                            <SongIcon/>
+                                        </Col>
+                                        <Col className="songInfoBox">
+                                            <Paragraph className="songInfo songTitle">{song.name}</Paragraph>
+                                            <Paragraph className="songInfo">{song.author}</Paragraph>
+                                        </Col>
+                                    </Row>
+                                </Link>
+                            </li>
                         ))}
                     </ul>
                 </Col>
             </Row>
+
+            <WaveIcon className="addButton" pos="bottom left" onClick={handleNavigate} icon={<AddIcon/>}/>
         </>
     );
 };
