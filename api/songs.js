1f30ffb4baf8739d62189c9bcdc1e777f4f2d015
Sue Hovhan <87306378+SyuneHovan@users.noreply.github.com>
2025-08-04 11:42:37 +0000

    CORS

--- api/songs.js
+++ api/songs.js
@@ -1,6 +1,17 @@
 import { Pool } from 'pg';
 
-export default async function handler(req, res) {
+export default async function handler(req, res) { 
+  // --- Add this block to allow requests from any origin ---
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  // --- End of CORS block ---
+
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
